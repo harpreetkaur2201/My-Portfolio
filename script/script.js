cea3323e47a609341d98a2fa5380a5f156f0401f
Harpreet <ksfibri@gmail.com>
2025-11-13 23:30:28 -0600

    My portfolio

---
+++ script/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
